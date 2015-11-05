37fc2d04627107ce2664d671c72df72679f1ae4a
John D. Gaffney <john.gaffney@isobar.com>
2015-11-05 14:12:16 -0500

    updated the stubFunctions to get data from the json files

--- data/stubFunctions.js
+++ data/stubFunctions.js
@@ -1,24 +1,28 @@
-/**
- * Created by ahuffman on 8/17/15.
+ /**
+ * Created by John D. Gaffney on 11/05/15.
  */
-let patientDemographicData = require('./patientDemographics.json');
-let patientAddressData = require('./patientAddresses.json');
-let patientMedicalStatusesData = require('./patientMedicalStatuses.json');
-let financePrograms = require('./patientFinancePrograms.json')
+let playerData = require('./player.json');
+let matchData = require('./match.json');
+let scoreData = require('./score.json')
+let tryData = require('./try.json');
 
-export const getPatientFromFile = (patientId)=> patientDemographicData.filter((patient)=> patient.id == patientId)[0]
+ export const getAllPlayerDataFromFile = () => playerData;
+ export const getPlayerDataFromFile = (playerId)=> playerData.filter(function(player) {
+  console.log(player);
+  console.log(playerId);
+  return player.playerId === playerId;
+ })
 
-export const getPatientsFromFile = (limit)=> limit == null || limit > patientDemographicData.length ? patientDemographicData : patientDemographicData.slice(0, limit)
+// export const getPatientsFromFile = (limit)=> limit == null || limit > patientDemographicData.length ? patientDemographicData : patientDemographicData.slice(0, limit)
 
-//export const getPatientAddressFromFile = (patientId)=> patientAddressData.filter((address) => address.patientId == patientId)[0]
+// export const getPatientAddressFromFile = (patientId)=> patientAddressData.filter((address) => address.patientId == patientId)[0]
 
-export const getPatientAddressesFromFile = (patientIds)=> patientAddressData.filter((address) => patientIds.indexOf(address.patientId) != -1)
+// export const getPatientAddressesFromFile = (patientIds)=> patientAddressData.filter((address) => patientIds.indexOf(address.patientId) != -1)
 
-export const getPatientMedicalStatusFromFile = (patientId)=> patientMedicalStatusesData.filter((medStatus)=> medStatus.patientId == patientId)[0]
+// export const getPatientMedicalStatusFromFile = (patientId)=> patientMedicalStatusesData.filter((medStatus)=> medStatus.patientId == patientId)[0]
 
-export const getPatientMedicalStatusesFromFile = (patientIds)=> patientMedicalStatusesData.filter((medStatus)=> patientIds.indexOf(medStatus.patientId) != -1 )
+// export const getPatientMedicalStatusesFromFile = (patientIds)=> patientMedicalStatusesData.filter((medStatus)=> patientIds.indexOf(medStatus.patientId) != -1 )
 
-export const getPatientFinanceProgramFromFile = (patientId)=> financePrograms.filter((financeProgram)=> financeProgram.patientId==patientId )
+// export const getPatientFinanceProgramFromFile = (patientId)=> financePrograms.filter((financeProgram)=> financeProgram.patientId==patientId )
 
-export const getPatientFinanceProgramsFromFile = (patientIds)=> financePrograms.filter((finances)=> patientIds.indexOf(finances.patientId) != -1 )
-
+// export const getPatientFinanceProgramsFromFile = (patientIds)=> financePrograms.filter((finances)=> patientIds.indexOf(finances.patientId) != -1 )
